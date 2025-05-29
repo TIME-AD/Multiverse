a37d35c31f99bbedbea5b6bc0a724269d56d63f4
erinl-ferguson <104412546+erinl-ferguson@users.noreply.github.com>
2025-05-29 14:21:24 -0700

    Update 003_Compile_Results.R
    added small comments

--- 003_Compile_Results.R
+++ 003_Compile_Results.R
@@ -1,3 +1,5 @@
+# Purpose: This script compiled coefficients from all models into one place for ease of use. 
+
 rm(list=ls())
 library(reshape)
 library(glue)
@@ -6,7 +8,7 @@ library(dplyr)
 library(lubridate)
 library(rlist)
 
-# Change depending on your operating system
+# Set up directory (change depending on your operating system)
 project_dir = setwd("~/Dropbox/Github/TIME-AD/Multiverse")
 
 # Define controller class
@@ -17,21 +19,23 @@ project_name <- "Multiverse Workshop"
 controller <- readRDS(file.path("Results",project_name,"controller.RDS"))
 controller$init_libraries()
 
-#Load outputs
+# Create empty list to store estimates
 estimates <- list()
 controller$set_script("002")
 end_main_loop <- FALSE
 while(end_main_loop == FALSE){
+  #Load outputs
   estimates[[length(estimates)+1]] <- controller$load_project_data("estimates.RDS") %>%
     cbind(controller$get_current_row())
 
   estimates[[length(estimates)]]$coef <- rownames(estimates[[length(estimates)]])
-
+# Remove row names 
   rownames(estimates[[length(estimates)]]) <- NULL
   if(is.na(controller$next_spec())){
     end_main_loop <- TRUE
   }
 }
+# Bind list elements by row and save
 estimates <- list.rbind(estimates)
 
 controller$set_script("003")
